85440dd239922ae8d6e04b86598ab11f2f017c58
sungx <sungx@yugusoft.com>
2019-07-10 08:33:21 +0800

    调整

--- src/modules/space/components/store/Logs.vue
+++ src/modules/space/components/store/Logs.vue
@@ -4,27 +4,30 @@
       <no-data></no-data>
     </template>
     <template v-else>
-      <van-list
-        v-model="loading"
-        :finished="finished"
-        finished-text="没有更多数据了"
-        @load="onLoadMoreData">
-        <van-cell class="list-cell"
-          v-for="item in list"
-          :key="item.id">
-          <div class="item-content">
-            <div class="item-content-top">
-              <span>{{item.createDate | timesToDate('yyyy-MM-dd HH:mm')}}</span>
-              <span>余额：{{item.after}}云币</span>
+      <div class="log-list">
+        <van-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多数据了"
+          @load="onLoadMoreData">
+          <van-cell class="list-cell"
+                    v-for="item in list"
+                    :key="item.id">
+            <div class="item-content">
+              <div class="item-content-top">
+                <span>{{item.createDate | timesToDate('yyyy-MM-dd HH:mm')}}</span>
+                <span>余额：{{item.after}}云币</span>
+              </div>
+              <div class="item-content-bottom">
+                <span class="summary">{{item.summary}}</span>
+                <span v-if="item.type == 'CONSUME'"> -{{item.change}}云币</span>
+                <span v-if="item.type == 'RECHARGE'"> +{{item.change}}云币</span>
+              </div>
             </div>
-            <div class="item-content-bottom">
-              <span class="summary">{{item.summary}}</span>
-              <span v-if="item.type == 'CONSUME'"> -{{item.change}}云币</span>
-              <span v-if="item.type == 'RECHARGE'"> +{{item.change}}云币</span>
-            </div>
-          </div>
-        </van-cell>
-      </van-list>
+          </van-cell>
+        </van-list>
+      </div>
+
     </template>
   </div>
 </template>
@@ -83,18 +86,24 @@
 <style rel="stylesheet/less" lang="less" scoped>
   @import "~@/config/config.less";
   .charge-logs-container{
-    .list-cell{
-      .item-content{
-        display: flex;
-        flex-direction: column;
-        .item-content-top{
+    height: 100%;
+    background: @BG_WHITE;
+    .log-list{
+      height: 100%;
+      overflow-y: scroll;
+      .list-cell{
+        .item-content{
           display: flex;
-          align-items: center;
-          justify-content: space-between;
-        }
-        .item-content-bottom{
-          margin-top: 10px;
-          color: @FONT_THIRD_COLOR;
+          flex-direction: column;
+          .item-content-top{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
+          .item-content-bottom{
+            margin-top: 10px;
+            color: @FONT_THIRD_COLOR;
+          }
         }
       }
     }
