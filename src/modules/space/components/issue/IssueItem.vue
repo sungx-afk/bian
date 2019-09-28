3c3489c694606de071e033ec1a23fe656111dc0f
sungx <sungx@yugusoft.com>
2019-09-28 10:28:19 +0800

    调整图片显示

--- src/modules/space/components/issue/IssueItem.vue
+++ src/modules/space/components/issue/IssueItem.vue
@@ -49,12 +49,16 @@
           </div>
         </div>
         <div v-else-if="item.photos.length===5">
-          <div class='t5-1-view'>
-            <img class='t5-1-image1' :src='item.photos[0].thumbnailUrl || item.photos[0].url' @click.stop='imagePressed(0)' />
-            <img class='t5-1-image2' :src='item.photos[1].thumbnailUrl || item.photos[1].url' @click.stop='imagePressed(1)' />
-            <img class='t5-1-image3' :src='item.photos[2].thumbnailUrl || item.photos[2].url' @click.stop='imagePressed(2)' />
-            <img class='t5-1-image4' :src='item.photos[3].thumbnailUrl || item.photos[3].url' @click.stop='imagePressed(3)' />
-            <img class='t5-1-image5' :src='item.photos[4].thumbnailUrl || item.photos[4].url' @click.stop='imagePressed(4)' />
+          <div class='t5-5-view'>
+            <div class='t5-5-left'>
+              <img class='t5-5-image1' :src='item.photos[0].thumbnailUrl || item.photos[0].url' @click.stop='imagePressed(0)' />
+              <img class='t5-5-image2' :src='item.photos[1].thumbnailUrl || item.photos[1].url' @click.stop='imagePressed(1)' />
+              <img class='t5-5-image3' :src='item.photos[2].thumbnailUrl || item.photos[2].url' @click.stop='imagePressed(2)' />
+            </div>
+            <div class='t5-5-right'>
+              <img class='t5-5-image4' :src='item.photos[3].thumbnailUrl || item.photos[3].url' @click.stop='imagePressed(3)' />
+              <img class='t5-5-image5' :src='item.photos[4].thumbnailUrl || item.photos[4].url' @click.stop='imagePressed(4)' />
+            </div>
           </div>
         </div>
         <div v-else-if="item.photos.length===6">
@@ -393,49 +397,51 @@
         }
       }
     }
+    .t5-5-view
+    {
+      display:flex;
+      .t5-5-left
+      {
+        width: 34%;
+        margin-right:3px;
+        .t5-5-image1
+        {
+          width:100%;
+          height:105px;
+          object-fit: cover;
+        }
 
-    /* t5-1 */
-    .t5-1-view {
-      height:350px;
-      .t5-1-image1 {
-        width:122px;
-        height:230px;
-        position:relative;
-        object-fit: cover;
-      }
-      .t5-1-image2 {
-        left:6px;
-        width:250px;
-        bottom:122px;
-        height:107px;
-        position:relative;
-        object-fit: cover;
-      }
+        .t5-5-image2
+        {
+          width:100%;
+          height:110px;
+          object-fit: cover;
+        }
 
-      .t5-1-image3 {
-        top:-235px;
-        left:128px;
-        width:117px;
-        height:117px;
-        position:relative;
-        object-fit: cover;
+        .t5-5-image3
+        {
+          width:100%;
+          height:105px;
+          object-fit: cover;
+        }
       }
+      .t5-5-right
+      {
+        width: 66%;
+        margin-left:3px;
+        .t5-5-image4
+        {
+          width: 100%;
+          height: 220px;
+          object-fit: cover;
+        }
 
-      .t5-1-image4 {
-        top:-121px;
-        left:125px;
-        width:125px;
-        height:230px;
-        position:relative;
-        object-fit: cover;
-      }
-
-      .t5-1-image5 {
-        top:-235px;
-        width:246px;
-        height:107px;
-        position:relative;
-        object-fit: cover;
+        .t5-5-image5
+        {
+          width: 100%;
+          height: 105px;
+          object-fit: cover;
+        }
       }
     }
 
