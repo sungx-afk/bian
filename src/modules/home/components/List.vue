66eb1afde3809c785110aaf0fefbcca37f7538f8
sungx <sungx@yugusoft.com>
2020-03-09 22:27:38 +0800

    调整

--- src/modules/home/components/List.vue
+++ src/modules/home/components/List.vue
@@ -318,6 +318,9 @@
       loginWithCode(code){
         this.$store.dispatch('userStore/loginWithCode', {code})
       },
+      loginWithToken(token){
+        this.fetchMyInfo(token)
+      },
       loginWithUid(uid){
         console.log("===loginWithUid====");
         this.$store.dispatch('userStore/loginWithUid', {uid})
@@ -352,7 +355,7 @@
           }
         }
         if (token){
-          //已经跳转了
+          this.loginWithToken(token)
           return
         }
         if (uid){
