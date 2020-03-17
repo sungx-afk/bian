55d3171738541e6e2aff43e3d7e1ac72fa31da2d
sungx <sungx@yugusoft.com>
2020-03-17 11:46:11 +0800

    调整

--- src/modules/space/components/manage/Bgm.vue
+++ src/modules/space/components/manage/Bgm.vue
@@ -20,7 +20,7 @@
       </template>
       <van-button  class="paste-btn" icon="edit" size="small" @click="showPasteDialog">手动编辑</van-button>
       <div class="tip" v-if="!isShowUploader()">
-        由于iOS系统限制，您可以直接手动编辑输入音乐链接或者使用Android手机、网页端进行音乐文件上传
+        <i class="iconfont icon-tishi1"></i>由于iOS系统限制，您可以直接手动编辑输入音乐链接或者使用Android手机、网页端进行音乐文件上传
       </div>
     </van-cell-group>
     <van-popup class="popup-area" v-model="isShowPasteDialog" @closed="pasteDialogClosed">
@@ -205,6 +205,11 @@
       padding: 16px;
       font-size: 12px;
       color: @FONT_THIRD_COLOR;
+      .iconfont{
+        font-size: 14px;
+        color: @FONT_THIRD_COLOR;
+        margin-right: 4px;
+      }
     }
     .popup-area{
       .popup-cell{
