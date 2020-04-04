8a0bb1a67bfe19e86cbf0f9979e4d92f080d72c5
sungx <sungx@yugusoft.com>
2020-04-04 10:50:08 +0800

    调整

--- src/modules/space/components/meeting/Meeting.vue
+++ src/modules/space/components/meeting/Meeting.vue
@@ -17,9 +17,17 @@
       <div class="tip">
         <i class="iconfont icon-tishi1"></i>为刚去世的亲人创建电子讣告并发送给他人，对方点击后可进行在线祭奠。
       </div>
-      <div class="report-area">
+      <div class="report-area" v-if="spaceUsers">
         <div class="report-header">
           <div class="title">讣告</div>
+        </div>
+        <div class="avatar-wrapper" v-if="avatarChecked">
+          <img :src="avatarUrl"  v-if="avatarUrl"/>
+          <div v-else class="avatar-fake"></div>
+          <div class="loading">
+            <van-loading  v-if="uploadAvatarLoading" vertical color="#FFFFFF">上传中...</van-loading>
+          </div>
+
         </div>
         <van-field
           v-model="deathNotice"
@@ -29,9 +37,18 @@
           :autosize="{ maxHeight: maxH, minHeight: minH }"
           @blur="noticeInputBlur">
         </van-field>
+        <div class="avatar-operate">
+          <van-checkbox class="show-avatar" v-model="avatarChecked" name="avatar" shape="square" checked-color="#825621">显示遗像</van-checkbox>
+          <template v-if="avatarChecked">
+            <van-button class="switch-btn" size="small" v-if="showSwitchBtn" @click="switchAvatar">切换遗像</van-button>
+            <van-uploader :after-read="afterSelectPhoto">
+              <van-button class="upload-btn" size="small">重新上传</van-button>
+            </van-uploader>
+          </template>
+        </div>
       </div>
       <div class="poster-btn">
-        <van-button @click.stop="makePoster" type="default">发给亲友</van-button>
+        <van-button @click.stop="makePoster" type="default">下一步</van-button>
       </div>
     </template>
 
@@ -41,6 +58,11 @@
 <script>
   import {mapGetters} from 'vuex';
   import config_server from '@/config/config'
+
+  import {timesToDate,Link,gUuid} from '@/config/utils'
+
+  import constant from '@/config/constant'
+
   import base64 from 'js-base64'
 
   const FONT = "px bold Pingfang SC,STHeiti,Lantinghei SC,Open Sans,Arial,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,SimSun,sans-serif"
@@ -50,11 +72,11 @@
       data(){
         return{
           spaceId:'',
-          spaceDetail:null,
+          spaceUsers:null,
           deathNotice:'',
           qrCode:'',
           rows:10,
-          maxH:400,
+          maxH:200,
           minH:200,
           posterW:0,
           posterH:0,
@@ -63,61 +85,115 @@
           showPoster:false,
           logoDrawDone:false,
           qrCodeDrawDone:false,
-          posterDone:false
+          avatarDrawDone:false,
+          posterDone:false,
+          avatarChecked:true,
+          avatarUrl:'',
+          uploadAvatarLoading:false
         }
       },
       computed: {
         ...mapGetters({
           user: 'userStore/user',
         }),
+        showSwitchBtn(){
+          let result = false
+          if (this.spaceUsers.length > 1 && this.spaceUsers.filter(user=>user.avatarUrl).length > 1){
+            result = true
+          }
+          return result
+        }
       },
       watch:{
         logoDrawDone(newVal,oldVal){
           if (!newVal){
             return
           }
-          if (this.qrCodeDrawDone){
-            this.generateImage()
+          if (this.avatarChecked){
+            if (this.qrCodeDrawDone && this.avatarDrawDone){
+              this.generateImage()
+            }
+          }else {
+            if (this.qrCodeDrawDone){
+              this.generateImage()
+            }
           }
         },
         qrCodeDrawDone(newVal,oldVal){
           if (!newVal){
             return
           }
-          if (this.logoDrawDone){
+          if (this.avatarChecked){
+            if (this.logoDrawDone && this.avatarDrawDone){
+              this.generateImage()
+            }
+          }else {
+            if (this.logoDrawDone){
+              this.generateImage()
+            }
+          }
+
+        },
+        avatarDrawDone(newVal,oldVal){
+          if (!newVal){
+            return
+          }
+          if (this.logoDrawDone && this.qrCodeDrawDone){
             this.generateImage()
           }
-        },
+        }
       },
       methods:{
         getSpaceDetail(cb){
           $API.space.getSpaceDetail({
             sid:this.spaceId
           }, rsp=>{
-            this.spaceDetail = rsp
+            this.spaceUsers = rsp.spaceUsers
+            this.avatarUrl = this.spaceUsers[0].avatarUrl
             cb && cb()
           })
         },
         initDeathNotice(){
           let notice = ''
-          if (this.spaceDetail && this.spaceDetail.spaceUsers && this.spaceDetail.spaceUsers.length > 0){
-            if (this.spaceDetail.spaceUsers[0].deathNotice){
-              notice = this.spaceDetail.spaceUsers[0].deathNotice
+          if (this.spaceUsers[0]){
+            if (this.spaceUsers[0].deathNotice){
+              notice = this.spaceUsers[0].deathNotice
             }
           }
-          if (!notice){
-            this.deathNotice = `xxx 同志因 xx 不幸于 xxxx 年 xx 月 xx 日在 xx 市逝世，享年 xx 岁。葬礼遵其遗愿，一切从简，特此讣告。
+          if (notice && false){
+            this.deathNotice = notice
+          }else{
+            if (this.spaceUsers && this.spaceUsers.length === 1){
+              let spaceUser = this.spaceUsers[0]
+              let content = ''
+              let dieDate = spaceUser.dieDay? timesToDate(spaceUser.dieDay,'yyyy 年 MM 月 dd 日') : 'xxxx 年 xx 月 xx 日'
+              let age = 'xx'
+
+              if (spaceUser.birthday && spaceUser.dieDay){
+                age = new Date(spaceUser.dieDay).getFullYear() - new Date(spaceUser.birthday).getFullYear() + 1
+              }
+
+              content += `${spaceUser.name}`
+              content += spaceUser.sex?'女士':'先生'
+              content += '因 xx 不幸于 '
+              content += dieDate
+              content += `在 xx 市逝世，享年 ${age} 岁。葬礼遵其遗愿，一切从简，特此讣告。
 
 xxx`
-          }else{
-            this.deathNotice = notice
+
+              this.deathNotice = content
+            }else{
+              this.deathNotice = `xxx 先生/女士因 xx 不幸于 xxxx 年 xx 月 xx 日在 xx 市逝世，享年 xx 岁。葬礼遵其遗愿，一切从简，特此讣告。
+
+xxx`
+            }
           }
         },
         noticeInputBlur(){
           this.updateDeathNotice()
         },
         updateDeathNotice(){
-          let userId = this.spaceDetail.spaceUsers[0].id
+          let userId = this.spaceUsers[0].id
           $API.space.updateSpaceUserNotice({
             sid: this.spaceId,
             userId: userId,
@@ -126,11 +202,75 @@ xxx`
 
           })
         },
+        switchAvatar(){
+          let index = this.spaceUsers.findIndex(item=>item.avatarUrl === this.avatarUrl)
+          if (index === -1){
+            index = 0
+          }
+          if (index > 0){
+            this.avatarUrl = this.spaceUsers[index-1].avatarUrl
+          }else {
+            this.avatarUrl = this.spaceUsers[index+1].avatarUrl
+          }
+        },
+        afterSelectPhoto(photo){
+          let data = {}
+          data.identifier = this.identifier = gUuid()
+          data.content = photo.content
+          data.name = photo.file.name
+          data.size = photo.file.size
+          data.type = photo.file.type
+          data.lastModified = photo.file.lastModified
+          this.$store.dispatch('spaceStore/setCropImageData',data)
+          this.$nextTick(()=>{
+            Link(`/cropper`)
+          })
+        },
+        updateAvatarData(result){
+          let that = this
+          let info = result.info
+
+          if (info.identifier !== this.identifier){
+            return
+          }
+
+          let cropperData = result.cropperData
+          that.avatarUrl = result.cropperData
+          that.uploadAvatarLoading = false
+
+          $API.space.filesQiniuUploadTicket({
+            reqType: 'general_file',
+            name: info.name,
+            expand: info.name.replace(/.+\./, ''),
+            size: info.size,
+          }, resp => {
+            $API.space.filesQiniuUpload({
+              data:cropperData,
+              token:resp.uptoken,
+              key:resp.key
+            },rsp=>{
+              that.avatarUrl = rsp.url
+              that.uploadAvatarLoading = false
+            },error=>{
+              this.$toast("上传失败，请稍后重试")
+              that.uploadAvatarLoading = false
+            })
+          },error=>{
+            this.$toast("上传失败，请稍后重试")
+            that.uploadAvatarLoading = false
+          })
+        },
         initPosterWH(){
           this.posterW = document.documentElement.clientWidth - 50
           this.posterH = document.documentElement.clientHeight - 150
         },
         makePoster(){
+
+          if (this.avatarChecked && !this.avatarUrl){
+            this.$toast("请上传遗像或不使用遗像")
+            return
+          }
+
           this.showPoster = true
           this.initPosterWH()
           this.$nextTick(()=>{
@@ -141,6 +281,7 @@ xxx`
           this.showPoster = false
           this.logoDrawDone = false
           this.qrCodeDrawDone = false
+          this.avatarDrawDone = false
           this.posterDone = false
           this.context = null
         },
@@ -170,6 +311,7 @@ xxx`
           })
           that.drawBg()
           that.drawTitle()
+          that.drawAvatar()
           that.drawContent()
           that.drawLogo()
           that.drawTip()
@@ -191,6 +333,27 @@ xxx`
           this.context.fillStyle = '#000000'
           this.context.fillText(title, tx, ty);
         },
+        drawAvatar(){
+
+          if (!this.avatarChecked){
+            return
+          }
+          let lw = 122
+          let lh = 157
+          let lx = (this.posterW - lw)/2
+          let ly = 60
+
+          let that = this
+
+          let img = new Image()
+          img.setAttribute("crossOrigin",'Anonymous');
+          img.src = this.avatarUrl
+
+          img.onload = ()=>{
+            this.context.drawImage(img,lx,ly,lw, lh)
+            that.avatarDrawDone = true
+          }
+        },
         drawContent(){
           if (this.deathNotice){
             let fontSize = 14
@@ -203,6 +366,9 @@ xxx`
             let content = this.deathNotice
             let lines = content.split('\n')
             let prevY = 60
+            if (this.avatarChecked){
+              prevY += 160
+            }
             for (let i = 0; i < lines.length; i++) {
               let line = lines[i]
               let x = 40/2
@@ -335,6 +501,10 @@ xxx`
             this.initDeathNotice()
           })
         }
+        eventHub.$on(constant.EVENT_IMAGE_CROP_COMPLETE,this.updateAvatarData)
+      },
+      beforeDestroy() {
+        eventHub.$off(constant.EVENT_IMAGE_CROP_COMPLETE,this.updateAvatarData)
       }
     }
 </script>
@@ -347,6 +517,8 @@ xxx`
     align-items: center;
     flex-direction: column;
     background-color: #f6f6f6;
+    overflow-y: auto;
+    padding-bottom: 32px;
     .tip{
       font-size:14px;
       padding:10px;
@@ -368,6 +540,42 @@ xxx`
         .title{
           font-size: 16px;
           font-weight: bold;
+        }
+      }
+      .avatar-wrapper{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+        img{
+          width: 122px;
+          height: 157px;
+        }
+        .avatar-fake{
+          width: 122px;
+          height: 157px;
+          border: 2px solid @MAIN_THEME_COLOR;
+        }
+        .loading{
+          position: absolute;
+          margin-top: 60px;
+          /deep/.van-loading__text{
+            color: #FFFFFF;
+          }
+        }
+      }
+      .avatar-operate{
+        display: flex;
+        align-items: center;
+        padding: 20px;
+        .show-avatar{
+          /deep/.van-checkbox__label{
+            font-size: 13px;
+            color: @FONT_THIRD_COLOR;
+          }
+        }
+        .switch-btn,.upload-btn{
+          margin: 0px 8px;
         }
       }
     }
@@ -422,7 +630,8 @@ xxx`
         position: absolute;
         top: 10px;
         right: 10px;
-        color: #a9a9a9;
+        font-size: 20px;
+        color: @MAIN_THEME_COLOR;
       }
       .poster-hidden{
         display: none;
