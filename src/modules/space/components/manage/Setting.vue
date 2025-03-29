700905b8519fd5680abb880b71e10f90457b73cb
sungx <sungx@yugusoft.com>
2025-03-29 09:27:28 +0800

    调整

--- src/modules/space/components/manage/Setting.vue
+++ src/modules/space/components/manage/Setting.vue
@@ -22,6 +22,34 @@
       <van-cell title="移交纪念馆" is-link @click.stop="goTransferSpace"></van-cell>
       <van-cell title="删除纪念馆" clickable @click.stop="deleteSpace"></van-cell>
     </van-cell-group>
+    <van-dialog 
+      v-model="showDeleteDialog" 
+      closeOnClickOverlay
+      show-cancel-button
+      className='custom-delete-dialog'
+      confirmButtonText='取消'
+      cancelButtonText='删除'
+      :beforeClose="beforeCloseHandler"
+      @confirm="cancelDelete">
+      <div class="delete-box">
+        <div class="tip">删除纪念馆会导致纪念馆所有数据丢失，请谨慎操作。请输入下面的四位数字后进行删除。</div>
+        <div class="random">{{ randomStr }}</div>
+        <div class="confirm-input">
+          <van-password-input
+            :value="confirmValue"
+            :length="4"
+            :mask="false"
+            :error-info="errorInfo"
+            :focused="showKeyboard"
+            @focus="showKeyboard = true"
+          />
+        </div>
+      </div>
+    </van-dialog>
+    <van-number-keyboard class="custom-number-keyboard"
+      v-model="confirmValue"
+      :show="showKeyboard"
+      @blur="showKeyboard = false"/>
   </div>
 </template>
 
@@ -29,6 +57,14 @@
   import {Link} from '@/config/utils'
 
   import constant from '@/config/constant'
+
+  import Vue from 'vue';
+  import { Dialog,PasswordInput, NumberKeyboard  } from 'vant';
+
+  // 全局注册
+  Vue.use(Dialog);
+  Vue.use(PasswordInput);
+  Vue.use(NumberKeyboard);
 
     export default {
       name: "Setting",
@@ -41,7 +77,12 @@
       data(){
         return{
           viewScopeCheck:false,
-          commentScopeCheck:false
+          commentScopeCheck:false,
+          showDeleteDialog:false,
+          confirmValue:'',
+          showKeyboard:false,
+          errorInfo:'',
+          randomStr:''
         }
       },
       methods:{
@@ -101,24 +142,43 @@
           Link(`/space/transfer/${this.space.id}`)
         },
         deleteSpace(e){
-          let that = this
-          this.$dialog.confirm({
-            className:'my-delete-dialog',
-            confirmButtonText:'取消',
-            cancelButtonText:'删除',
-            message: `删除纪念馆会导致纪念馆所有数据丢失，请谨慎操作。确认删除该纪念馆吗?`
-          }).then(() => {
-            // cancel //删除操作特殊处理到左侧按钮
-          }).catch(() => {
+          let random = Math.floor(Math.random() * 9000) + 1000
+          this.randomStr = random.toString()
+          this.showDeleteDialog = true
+          this.showKeyboard = true
+        },
+        resetDeleteInfo(){
+          this.confirmValue = ''
+          this.showKeyboard = false,
+          this.randomStr = ''
+          this.errorInfo = ''
+        },
+        cancelDelete(){
+          this.resetDeleteInfo()
+        },
+        beforeCloseHandler(action, done){
+          if (action == 'confirm' || action == 'overlay'){
+            this.resetDeleteInfo()
+            done()
+          }else{
+            //判断是否匹配上了
+            if (this.confirmValue != this.randomStr){
+              this.errorInfo = '输入数字未匹配'
+              done(false) 
+              return
+            }
             $API.space.deleteSpace({
-                sid:that.space.id
-              }, rsp=>{
-                eventHub.$emit(constant.EVENT_DELETE_SPACE_SUCCESS,that.space.id)
-              this.$router.go(-2)
+              sid:this.space.id
+            }, rsp=>{
+              this.resetDeleteInfo()
+              done()
+              eventHub.$emit(constant.EVENT_DELETE_SPACE_SUCCESS,this.space.id)
+              //直接返回到列表上去
+              Link(`/list`,{},true)
               }, error=>{
                 this.$toast('删除失败，请稍后重试')
             })
-          })
+          }
         },
       },
       created() {
@@ -137,11 +197,42 @@
 
 </style>
 
-<style>
-  .my-delete-dialog .van-dialog__footer .van-dialog__cancel{
-    color: red !important;
+<style rel="stylesheet/less" lang="less">
+  .custom-delete-dialog .van-dialog__footer .van-dialog__cancel{
+    color: #ee0a24 !important;
   }
-  .my-delete-dialog .van-dialog__footer .van-dialog__confirm{
+  .custom-delete-dialog .van-dialog__footer .van-dialog__confirm{
     color: #666666 !important;
   }
+  .custom-delete-dialog{
+    .delete-box{
+      padding: 20px;
+      .tip{
+        font-size: 14px;
+        color: #333333;
+      }
+      .random{
+        margin-top: 12px;
+        font-size: 20px;
+        text-align: center;
+      }
+      .confirm-input{
+        margin-top: 12px;
+        .van-password-input__security{
+          &::after{
+            border-color: #aaaaaa;
+          }
+        }
+        .van-password-input__item{
+          &::after{
+            border-color: #aaaaaa;
+          }
+        }
+      }
+    }
+  }
+  .custom-number-keyboard{
+    z-index: 999999 !important;
+  }
+  
 </style>
